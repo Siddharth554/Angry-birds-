dd45157d546cd0ea5515a6ff3b79dedb3cc97347
“Siddharth” <siddharthonkeyboard@gmail.com>
2021-01-19 16:53:18 +0530

    All files are here

--- Slingshot.js
+++ Slingshot.js
@@ -1,9 +1,9 @@
-class SlingShot{
-    constructor(bodyA, pointB){
+class SlingShot {
+    constructor(bodyA, pointB) {
         var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 0.04,
+            stiffness: 0.1,
             length: 10
         }
         this.sling1 = loadImage('sprites/sling1.png');
@@ -14,35 +14,40 @@ class SlingShot{
         World.add(world, this.sling);
     }
 
-    fly(){
+    fly() {
         this.sling.bodyA = null;
     }
 
-    display(){
-        image(this.sling1,200,20);
-        image(this.sling2,170,20);
-        if(this.sling.bodyA){
+    attach(body1) {
+        this.sling.bodyA = body1;
+
+    }
+
+    display() {
+        image(this.sling1, 200, 20);
+        image(this.sling2, 170, 20);
+        if (this.sling.bodyA) {
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             push();
-            
-            stroke(48,22,8);
-            if(pointA.x < 220) {
+
+            stroke(48, 22, 8);
+            if (pointA.x < 220) {
                 strokeWeight(7);
-                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x - 10, pointB.y);
                 line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
-                image(this.sling3,pointA.x -30, pointA.y -10,15,30);
+                image(this.sling3, pointA.x - 30, pointA.y - 10, 15, 30);
             }
-            else{
+            else {
                 strokeWeight(3);
-                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x - 10, pointB.y);
                 line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
-                image(this.sling3,pointA.x + 25, pointA.y -10,15,30);
+                image(this.sling3, pointA.x + 25, pointA.y - 10, 15, 30);
             }
-           
-            
+
+
             pop();
         }
     }
-    
+
 }
